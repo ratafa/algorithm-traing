f88785897de10e60c3a9c328895f3eb51d4e6b7e
youngchan Kim <aacz12035@gmail.com>
2024-01-18 11:36:43 +0900

    feat:solve 2559

---
+++ cpp/2559.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minimum = -10000000;
+int n,k,s, psum[100001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++){
+        cin >> s;
+        psum[i] += psum[i-1] + s;
+    }
+	for(int i = k; i <= n; i++){
+		minimum = max(minimum, psum[i] - psum[i - k]);
+	}
+    cout << minimum;
+    return 0;
+}
--- cpp/test/test.cpp
+++ cpp/test/test.cpp
@@ -1,27 +1,21 @@
-// 1-J
+// 1-H
 #include<bits/stdc++.h>
 using namespace std;
 
-int a, b, c, cnt;
+int minimum = -10000000;
+int n,k,s, psum[100001];
 
 int main() {
     ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-    cin >> a >> b;
-    int arr[a];
-    for(int i=0; i<a; i++){
-        cin >> c;
-        arr[i] = c;
+    cin.tie(NULL);cout.tie(NULL);
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++){
+        cin >> s;
+        psum[i] += psum[i-1] + s;
     }
-    for(int j=0; j<(a-1); j++){
-        for(int k=j+1; k<a; k++) {
-            if(arr[j]+ arr[k] == b){
-                cnt++;
-            }
-        }
-    }
-
-    cout << cnt;
+	for(int i = k; i <= n; i++){
+		minimum = max(minimum, psum[i] - psum[i - k]);
+	}
+    cout << minimum;
     return 0;
 }
