529101ef65c098da154b52981ccbe19199e51755
youngchan Kim <aacz12035@gmail.com>
2024-01-22 01:18:48 +0900

    feat: solve 4375

---
+++ cpp/4375.cpp
@@ -0,0 +1,24 @@
+// 1-O
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+
+    while(cin >> n){
+        int m = 1, k = 1;
+        while(1){
+            if(m % n == 0) {
+                cout << k << '\n';
+                break;    
+            }
+            m = m * 10 + 1;
+            m %= n;
+            k++;
+        }
+    }
+    return 0;
+}
