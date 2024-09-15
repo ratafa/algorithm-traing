2bcbbe82253ab9849f1630aca70ecd95a607d443
youngchan Kim <aacz12035@gmail.com>
2024-09-15 15:33:42 +0900

    [level 1] Title: 옹알이 (2), Time: 0.07 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+
+    for(int i;i<babbling.size();i++)
+    {
+        string temp1="";
+        string temp2="";
+        for(char c:babbling[i])
+        {
+            temp1+=c;
+            if(temp1 == "aya"||temp1 == "ye"||temp1 == "woo"||temp1 == "ma")
+            {
+                if(temp2 == temp1) break;
+                temp2=temp1;
+                temp1="";
+            }
+        }
+        if(temp1.size()==0) answer++;
+    }
+    return answer;
+}
+
+
+
+
+
+
+
